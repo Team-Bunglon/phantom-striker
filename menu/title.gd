74822460586734717600627728cd7efd6c5d93c7
rayhan-ryu <109362263+rayhan-ryu@users.noreply.github.com>
2024-04-24 18:45:05 +0700

    Add secret debug button

--- menu/title.gd
+++ menu/title.gd
@@ -7,3 +7,6 @@ func _input(event):
 	if event.is_action_pressed("start_game"):
 		Global.death_count = 0
 		get_tree().change_scene_to_file("res://levels/level1.tscn")
+	elif event.is_action_pressed("debug"):
+		Global.death_count = 0
+		get_tree().change_scene_to_file("res://levels/level0.tscn")
