a300e25df57971b6b0269df7a750f12ab4d6e1a9
ivanrbnc <ivanrabbanicool@gmail.com>
2024-05-02 00:05:29 +0700

    feat: disintegrating platform & initial for level 15 16

---
+++ objects/disintegratingplatform.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+var time = 1
+
+func _ready():
+	set_process(false)
+	
+func _process(delta):
+	time += 1
+	$AnimatedSprite.play()
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		set_process(true)
+		$Timer.start(1)
+
+func _on_timer_timeout():
+	queue_free()
