0f3fc3127617abddc51b155c38cb362954155740
KingGedorah <naufal.zhafari@ui.ac.id>
2024-05-10 16:03:25 +0700

    add level 29 & 30, modify level 7,8,17,18 and add moving platform to strikable tiles

--- objects/tutorialbox.gd
+++ objects/tutorialbox.gd
@@ -3,20 +3,14 @@ extends Area2D
 
 func _on_body_entered(body):
 	if (body.name == "Player"):
-		print("player entered")
 		var message_box = get_message_box()
 		if message_box:
-			print("show textbox")
 			message_box.get_node("MessageBoxContainer").show()
-			print(message_box.get_node("MessageBoxContainer/MarginContainer/VBoxContainer/Text1").text)
-			print(message_box.get_node("MessageBoxContainer/MarginContainer/VBoxContainer/Text2").text)
 
 func _on_body_exited(body):
 	if (body.name == "Player"):
-		print("player exit")
 		var message_box = get_message_box()
 		if message_box:
-			print("hide textbox")
 			message_box.get_node("MessageBoxContainer").hide()
 
 func get_message_box():
