566e3a2ce166bb0f4cb54f60d39f86e0c3404bc3
rayhan-ryu <109362263+rayhan-ryu@users.noreply.github.com>
2024-05-13 15:26:38 +0700

    Add better moving hazard

--- objects/movinghazardpoint.gd
+++ objects/movinghazardpoint.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-## A more flexible version of the previous MovingPlatform
-class_name MovingPlatformPoint
+## A more flexible version of the previous MovingHazard
+class_name MovingHazardPoint
 
 @export var direction: Vector2 ## The direction is relative from the starting position (where you placed your object)
 @export var speed: float = 100.0
@@ -10,7 +10,14 @@ class_name MovingPlatformPoint
 @onready var end_point := global_position + direction
 @onready var towards_end := true
 
+var hit_player := false
+
 func _physics_process(delta):
+	if hit_player:
+		$Sprite2D.rotation = 0.0
+		return
+	$Sprite2D.rotation += speed * delta
+	print($Sprite2D.rotation)
 	if towards_end:
 		global_position.x = move_toward(global_position.x, end_point.x, speed * delta)
 		global_position.y = move_toward(global_position.y, end_point.y, speed * delta)
@@ -22,3 +29,7 @@ func _physics_process(delta):
 		if global_position == start_point:
 			towards_end = true
 
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		hit_player = true
+		body.die()
