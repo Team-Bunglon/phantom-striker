d34574c6ca8a9c477cc97c770b0b7cf651d8d6f0
rayhan-ryu <109362263+rayhan-ryu@users.noreply.github.com>
2024-05-18 12:53:44 +0700

    Level Adjustment 28 + Tweak Destroy+Disintegrate

--- objects/destroyable_platform.gd
+++ objects/destroyable_platform.gd
@@ -10,26 +10,30 @@ class_name DestroyablePlatform
 var spike_node: TileMap
 var red_spike_node: TileMap
 
-var tiles_check: Dictionary = {} ## Check if the tile is already be checked during the cluster calculation
-var tiles_orig: Dictionary = {}
-var area_tile: Dictionary = {}
+var tiles_check: Dictionary = {} ## Check if the tile is already be checked during the cluster calculation.
+var tiles_orig: Dictionary = {}  ## The sprite of the tile before being destroyed.
+var tiles_area: Dictionary = {}  ## The Area2D of the tile during the broken state.
 
 var cluster: Array[Vector2i]
-var cluster_respawn: Dictionary = {}
+var cluster_entered: Dictionary = {}
 var cluster_tiles_entered: Dictionary = {}
 
-signal test()
+## Respawn the tiles inside a cluster. Emitted the player leaves the cluster's Area2D group.
+signal perform_respawn()
 
 func _ready():
 	if global_position != Vector2.ZERO:
 		assert(false, "DestroyablePlatform should have (0,0) as its position")
+
 	spike_node = get_node_or_null(spike_object)
 	red_spike_node = get_node_or_null(red_spike_object)
+
 	if not spike_object.is_empty():
 		for s in spike_node.get_used_cells(0):
 			if get_cell_source_id(0, s) != -1:
 				tiles_orig[s] = Vector2i(1,0)
 				set_cell(0, s, 0, tiles_orig[s])
+
 	if not red_spike_object.is_empty():
 		for s in red_spike_node.get_used_cells(0):
 			if get_cell_source_id(0, s) != -1:
@@ -37,19 +41,20 @@ func _ready():
 				set_cell(0, s, 0, tiles_orig[s])
 
 func _physics_process(_delta):
-	if not cluster.is_empty(): # Check if a cluster is formed
-		_break_cluster(cluster)
+	if not cluster.is_empty(): # Check if a cluster is formed. Should only be run in one frame.
+		var cluster_temp := cluster
 		cluster = []
+		_break_cluster(cluster_temp)
 
 ## The function to be called when being struck by the player. [br]
 ## It will form a cluster of destroyable platforms by 
-## checking its neighbors and function recursion.
-## A cluster must be formed in a single frame. 
-func break_platform(coord):
+## checking its neighbors and perform recursion.
+## A cluster must be formed within a single frame. 
+func break_platform(coord: Vector2i):
 	tiles_check[coord] = true
 	cluster.append(coord)
 	for g in get_surrounding_cells(coord):
-		if get_cell_source_id(0, g) != -1 or get_cell_source_id(1, g) != -1:
+		if get_cell_source_id(0, g) != -1:
 			if tiles_check.has(g):
 				if tiles_check[g] == false:
 					break_platform(g)
@@ -57,55 +62,62 @@ func break_platform(coord):
 				break_platform(g)
 
 ## Break the cluster
-func _break_cluster(cluster_array):
-	cluster_respawn[cluster_array] = true
+func _break_cluster(cluster_array: Array[Vector2i]):
+	cluster_entered[cluster_array] = false
 	cluster_tiles_entered[cluster_array] = {}
+
+	# Break tiles in a cluster
 	for c in cluster_array:
 		tiles_check[c] = false
 		_break_platform_procedure(c, cluster_array)
+
+	# Create timer and wait for respawn
 	await(get_tree().create_timer(respawn_timer, false).timeout)
-	if not cluster_respawn[cluster_array]:
-		await(self.test)
+	if cluster_entered[cluster_array]:
+		await(self.perform_respawn)
+
+	# Respawn tiles in a cluster
 	for c in cluster_array:
 		_respawn_platform_procedure(c)
 
 ## Function to change the tile atlas to a broken state
-func _break_platform_procedure(coord, cluster_array):
-	# Set some variables
+func _break_platform_procedure(coord: Vector2i, cluster_array: Array[Vector2i]):
 	cluster_tiles_entered[cluster_array][coord] = false
 	if not tiles_orig.has(coord):
 		tiles_orig[coord] = Vector2i(0,0)
 
 	# Set to broken state and Create area 2D to hold that state when the player is inside. 
 	Audio.play("Destroy")
-	_disintegrating_particle_create(coord)
 	set_cell(0, coord, 0, Vector2i(3,0))
-	area_tile[coord] = _create_area(coord, cluster_array)
+	_disintegrating_particle_create(coord)
+	tiles_area[coord] = _create_area(coord, cluster_array)
 
-func _respawn_platform_procedure(coord):
+## Respawn every platform inside a cluster
+func _respawn_platform_procedure(coord: Vector2i):
 	set_cell(0, coord, 0, tiles_orig[coord]) # Repawn procedure. If the player is still inside the tile, hold it until he leaves the area2D.
-	area_tile[coord].queue_free() # Delete objects and reset some variables
+	tiles_area[coord].queue_free()			 # Delete objects and reset some variables
 
+## Particle stuff
 func _disintegrating_particle_create(coord: Vector2i):
 	var disintegrating_particle: Disintegrating = disintegrating_particle_preload.instantiate()
 	var pos: Vector2 = coord * 16 + Vector2i(8, 8)
 	disintegrating_particle.emitting(pos)
-	print("destroyed at "+ str(coord))
 	get_parent().add_child(disintegrating_particle)
 
-func _create_area(coord, cluster_array) -> Area2D:
+## Factory function to create the area after the platform is destroyed.
+func _create_area(coord: Vector2i, cluster_array: Array[Vector2i]) -> Area2D:
 	var area: Area2D = area_preload.instantiate()
 	area.connect("body_entered", func(body): 
 		if body.name == "Player":
 			cluster_tiles_entered[cluster_array][coord] = true
-			cluster_respawn[cluster_array] = false
+			cluster_entered[cluster_array] = true
 		)
 	area.connect("body_exited", func(body): 
 		if body.name == "Player":
 			cluster_tiles_entered[cluster_array][coord] = false
 			if not cluster_tiles_entered[cluster_array].values().has(true):
-				cluster_respawn[cluster_array] = true
-				emit_signal("test")
+				cluster_entered[cluster_array] = false
+				emit_signal("perform_respawn")
 		)
 	area.global_position = map_to_local(coord)
 	add_child(area)
