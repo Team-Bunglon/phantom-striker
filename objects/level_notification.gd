9d939bcb145e80e96d2de8ac0de836087d55b4d5
ivanrbnc <ivanrabbanicool@gmail.com>
2024-05-21 22:46:28 +0700

    feat: level notification

---
+++ objects/level_notification.gd
@@ -0,0 +1,44 @@
+extends CanvasLayer
+class_name LevelBox
+
+#@export var line_one: String = "This is a sample text"
+#@export var line_two: String = "This is a sample text too"
+@export var opacity: float = 0.8 ## The opacity of the background panel
+
+@onready var textbox_container = $LevelNotificationContainer
+@onready var timer = $Timer
+
+func _ready():
+	visible = true
+	$LevelNotificationContainer/Panel.modulate.a = opacity
+	change_text()
+	hide_textbox()
+
+func hide_textbox():
+	textbox_container.hide()
+	
+func show_textbox():
+	textbox_container.show()
+
+func translate_group(level_number: int) -> String:
+	if (level_number >= 1 && level_number <= 10):
+		return "The Root"
+	elif (level_number >= 11 && level_number <= 20):
+		return "The Trunk"
+	elif (level_number >= 21 && level_number <= 30):
+		return "The Branch"
+	elif (level_number == 31):
+		return "The Fruit"
+	else:
+		return "Debug Mode"
+
+func change_text():
+	$LevelNotificationContainer/MarginContainer/VBoxContainer/Text1.text = translate_group(Global.current_level)
+	$LevelNotificationContainer/MarginContainer/VBoxContainer/Text2.text = str(Global.current_level) + "F"
+
+func appear():
+	show_textbox()
+	timer.start(1)
+
+func _on_timer_timeout():
+	hide_textbox()
